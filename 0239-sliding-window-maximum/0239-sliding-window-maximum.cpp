25c048667bcbf232d9db56de5dca3bf45dc4f713
Aditya <150531010+idityaGE@users.noreply.github.com>
2024-12-02 19:09:16 +0530

    Time: 32 ms (58.31%), Space: 139.1 MB (34.95%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -2,21 +2,23 @@ class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
         int n = nums.size();
-        
+
         deque <int> dq;
         vector<int> ans;
 
         for (int i = 0; i < n; i++) {
 
-            if (!dq.empty() && dq.front() <= i -k)
+            // removing after every k the element in deque
+            if (!dq.empty() && dq.front() <= i-k)
                 dq.pop_front();
 
+            // maintaining the monotonic stack
             while(!dq.empty() && nums[dq.back()] <= nums[i])
                 dq.pop_back();
 
             dq.push_back(i);
 
-            if( i >= k - 1)
+            if( i >= k - 1) // inserting start from k-1 index
                 ans.push_back(nums[dq.front()]);
 
         }
