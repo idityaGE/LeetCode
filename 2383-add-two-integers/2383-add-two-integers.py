08818790cc278fbd2437be3561996684a4600cb4
Aditya <150531010+idityaGE@users.noreply.github.com>
2024-12-16 00:04:23 +0530

    Time: 0 ms (100%), Space: 17.3 MB (9.22%) - LeetHub

--- 2383-add-two-integers/2383-add-two-integers.py
+++ 2383-add-two-integers/2383-add-two-integers.py
@@ -1,8 +1,4 @@
-class Solution(object):
-    def sum(self, num1, num2):
-        """
-        :type num1: int
-        :type num2: int
-        :rtype: int
-        """
+class Solution:
+    def sum(self, num1: int, num2: int) -> int:
         return num1+num2
+        
