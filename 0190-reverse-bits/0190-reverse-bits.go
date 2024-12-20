4e477f1a6543064a2febb96e06a78623fe5baca9
Aditya <150531010+idityaGE@users.noreply.github.com>
2024-12-20 23:18:27 +0530

    Time: 5 ms (18.81%), Space: 4.3 MB (49.28%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.go
+++ 0190-reverse-bits/0190-reverse-bits.go
@@ -1,8 +1,7 @@
 func reverseBits(num uint32) uint32 {
     res := uint32(0)
     for i := 0; i < 32; i++ {
-        res <<= 1
-        res += (num & 1)
+        res = (res << 1) | (num & 1)
         num >>= 1
     }
     return res
