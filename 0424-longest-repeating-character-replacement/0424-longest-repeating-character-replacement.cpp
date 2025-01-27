19272525bad54093814f846d6efd8210a7dc2775
Aditya <150531010+idityaGE@users.noreply.github.com>
2025-01-27 11:54:32 +0530

    Time: 0 ms (100%), Space: 10.9 MB (72.33%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,25 +1,22 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        map<char, int> mp;
-        int maxlen = 0;
-        int maxFreq = 0;
-        int l = 0;
+        int r=0,l=0,maxCount=0,maxLen=0;
+        int n = s.size();
+        int hash[26] ={0};
 
-        for (int r = 0; r < s.length(); r++) {
-            mp[s[r]]++;
-            maxFreq = max(maxFreq, mp[s[r]]);
+        while(r<n){
+            hash[s[r] -'A']++;
+            maxCount = max(maxCount,hash[s[r]-'A']);
 
-            if ((r - l + 1) - maxFreq > k) {
-                mp[s[l]]--;
-                if (mp[s[l]] == 0)
-                    mp.erase(s[l]);
+            if((r-l+1) -maxCount > k){
+                hash[s[l] -'A']--;
                 l++;
             }
 
-            maxlen = max(maxlen, r - l + 1);
+            maxLen = max(maxLen,r-l+1);
+            r++;
         }
-        
-        return maxlen;
+        return maxLen;
     }
 };
