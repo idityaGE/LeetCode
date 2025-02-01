50dedb9f536421f85cbe3d0d1bbd0974640bdb65
Aditya <150531010+idityaGE@users.noreply.github.com>
2025-02-01 23:18:15 +0530

    Time: 0 ms (100%), Space: 11.4 MB (81.59%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -6,27 +6,25 @@ public:
         vector<int> hash(128, 0);  
         for (char c : t) hash[c]++; 
 
-        int required = t.length();  // Total characters needed
-        int l = 0, r = 0;  // Two pointers
+        int required = t.length();
+        int l = 0, r = 0;
         int minLen = INT_MAX, startIdx = -1;
 
         while (r < s.length()) {
-            // If character at r is required, decrease count
             if (hash[s[r]] > 0) required--;
 
-            hash[s[r]]--;  // Reduce frequency in hash
-            r++;  // Expand window
+            hash[s[r]]--;
+            r++; 
 
-            while (required == 0) {  // When all characters are matched
-                // Update minimum window
+            while (required == 0) {
                 if (r - l < minLen) {
                     minLen = r - l;
                     startIdx = l;
                 }
 
-                hash[s[l]]++;  // Restore character at l
+                hash[s[l]]++;
                 if (hash[s[l]] > 0) required++;  
-                l++; 
+                l++;
             }
         }
 
