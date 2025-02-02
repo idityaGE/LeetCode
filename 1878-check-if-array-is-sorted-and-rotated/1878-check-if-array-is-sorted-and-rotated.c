52cc0ec078b2aaf80d16dc57b9dfa0f84d2fe16d
Aditya <150531010+idityaGE@users.noreply.github.com>
2025-02-02 15:31:38 +0530

    Time: 0 ms (100%), Space: 8.1 MB (31.36%) - LeetHub

---
+++ 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.c
@@ -0,0 +1,11 @@
+bool check(int* nums, int numsSize) {
+    int cnt = 0;
+    for (int i = 0; i < numsSize; i++) {
+        if (nums[i] > nums[(i + 1) % numsSize])
+            cnt++;
+
+        if (cnt > 1)
+            return false;
+    }
+    return true;
+}
