7707cd41972403276200c523f627aaa74864d5da
Aditya <150531010+idityaGE@users.noreply.github.com>
2025-02-06 18:21:48 +0530

    Time: 311 ms (64.89%), Space: 85.1 MB (45.45%) - LeetHub

--- 1364-tuple-with-same-product/1364-tuple-with-same-product.cpp
+++ 1364-tuple-with-same-product/1364-tuple-with-same-product.cpp
@@ -1,24 +1,16 @@
-#include <vector>
-#include <unordered_map>
-
 class Solution {
 public:
-    int tupleSameProduct(std::vector<int>& nums) {
-        std::unordered_map<int, int> freq;
+    int tupleSameProduct(vector<int>& nums) {
+        unordered_map<int, int> freq;
         int cnt = 0;
-        
-        for (size_t i = 0; i < nums.size(); i++) {
-            for (size_t j = i + 1; j < nums.size(); j++) {
+
+        for (size_t i = 0; i < nums.size(); i++)
+            for (size_t j = i + 1; j < nums.size(); j++)
                 freq[nums[i] * nums[j]]++;
-            }
-        }
-        
-        for (const auto& a : freq) {
-            int n = a.second;
-            if (n > 1) {
-                cnt += 8 * (n * (n - 1) / 2);
-            }
-        }
+
+        for (const auto& a : freq)
+            if (a.second > 1)
+                cnt += 8 * (a.second * (a.second - 1) / 2);
 
         return cnt;
     }
