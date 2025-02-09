a58e120b83fd24e081be7a1abb2ed810b2be0716
Aditya <150531010+idityaGE@users.noreply.github.com>
2025-02-09 15:16:44 +0530

    Time: 51 ms (15.38%), Space: 14.4 MB (61.54%) - LeetHub

---
+++ 2448-count-number-of-bad-pairs/2448-count-number-of-bad-pairs.go
@@ -0,0 +1,17 @@
+func countBadPairs(nums []int) int64 {
+    freq := make(map[int]int)
+    goodPairs := 0
+    n := len(nums)
+
+    for i, num := range nums {
+        key := num - i
+        if val, exists := freq[key]; exists {
+            goodPairs += val
+        }
+        freq[key]++
+    }
+
+    var badPairs int64 = int64(n*(n-1)/2 - goodPairs)
+
+    return badPairs
+}
