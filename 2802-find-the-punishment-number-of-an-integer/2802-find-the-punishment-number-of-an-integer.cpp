fb07887c286e534bc2d455a234b02361925e4565
Aditya <150531010+idityaGE@users.noreply.github.com>
2025-02-15 17:21:19 +0530

    Time: 0 ms (100%), Space: 8 MB (82.37%) - LeetHub

--- 2802-find-the-punishment-number-of-an-integer/2802-find-the-punishment-number-of-an-integer.cpp
+++ 2802-find-the-punishment-number-of-an-integer/2802-find-the-punishment-number-of-an-integer.cpp
@@ -1,17 +1,28 @@
 class Solution {
 public:
+    static inline bool partition(int x, int target) {
+        if (x == target)
+            return 1;
+        if (x == 0)
+            return target == x;
+
+        const int m0 = min(x, 1000);
+        [[unroll]]
+        for (int m = 10; m <= m0; m *= 10)
+            if (partition(x / m, target - x % m))
+                return 1;
+
+        return 0;
+    }
+
     int punishmentNumber(int n) {
-        vector<int> arr = {1,   9,   10,  36,  45,  55,  82,  91,  99,  100,
-                           235, 297, 369, 370, 379, 414, 657, 675, 703, 756,
-                           792, 909, 918, 945, 964, 990, 991, 999, 1000};
-
         int sum = 0;
-        for(const auto& i : arr) {
-            if (i <= n) sum += i*i;
-            else break;
+        for (int i = 1; i <= n; i++) {
+            if (i % 9 != 0 && i % 9 != 1)
+                continue;
+            const int x = i * i;
+            sum += (partition(x, i)) ? x : 0;
         }
-
         return sum;
-    
     }
 };
