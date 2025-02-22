670d527ae28405afeeb19dad673c261d2254efa7
Aditya <150531010+idityaGE@users.noreply.github.com>
2025-02-23 01:52:10 +0530

    Time: 0 ms (100%), Space: 4.8 MB (74.84%) - LeetHub

--- 1482-how-many-numbers-are-smaller-than-the-current-number/1482-how-many-numbers-are-smaller-than-the-current-number.go
+++ 1482-how-many-numbers-are-smaller-than-the-current-number/1482-how-many-numbers-are-smaller-than-the-current-number.go
@@ -1,12 +1,18 @@
 func smallerNumbersThanCurrent(nums []int) []int {
+    arr := make([]int, 101)
     res := make([]int, len(nums))
+    for _, v := range nums {
+        arr[v]++
+    }
+    for i := 1; i < 101; i++ {
+        arr[i] = arr[i] + arr[i - 1]
+    }
     for i := 0; i < len(nums); i++ {
-        c := 0
-        for j := 0; j < len(nums); j++ {
-            if j == i { continue }
-            if nums[j] < nums[i] { c++ }
+        if nums[i] == 0 {
+            res[i] = 0
+        } else {
+            res[i] = arr[nums[i] - 1]
         }
-        res[i] = c
     }
     return res
 }
