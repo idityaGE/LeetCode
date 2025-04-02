86ff44363270a1a989e22e03cb5fe3b339c5ba46
Aditya <150531010+idityaGE@users.noreply.github.com>
2025-04-03 00:03:18 +0530

    Time: 0 ms (100%), Space: 4.2 MB (16.85%) - LeetHub

---
+++ 0052-n-queens-ii/0052-n-queens-ii.go
@@ -0,0 +1,52 @@
+func totalNQueens(n int) int {
+	return solveNQueens(n)
+}
+
+func solveNQueens(n int) int {
+	board := make([]string, n)
+	for i := range board {
+		board[i] = strings.Repeat(".", n)
+	}
+	var result int
+	backtrack(0, &result, board, n)
+	return result
+}
+
+func backtrack(row int, result *int, board []string, n int) {
+	if row == n {
+		*result++
+		return
+	}
+
+	for col := 0; col < n; col++ {
+		if isValid(board, row, col, n) {
+			newRow := []byte(board[row])
+			newRow[col] = 'Q'
+			board[row] = string(newRow)
+
+			backtrack(row+1, result, board, n)
+
+			newRow[col] = '.'
+			board[row] = string(newRow)
+		}
+	}
+}
+
+func isValid(board []string, row, col, n int) bool {
+	for i := 0; i < row; i++ {
+		if board[i][col] == 'Q' {
+			return false
+		}
+	}
+	for i, j := row-1, col-1; i >= 0 && j >= 0; i, j = i-1, j-1 {
+		if board[i][j] == 'Q' {
+			return false
+		}
+	}
+	for i, j := row-1, col+1; i >= 0 && j < n; i, j = i-1, j+1 {
+		if board[i][j] == 'Q' {
+			return false
+		}
+	}
+	return true
+}
