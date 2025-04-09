ca01929162eeff560cb1860e65b362192eec3c84
Aditya <150531010+idityaGE@users.noreply.github.com>
2025-04-09 20:16:23 +0530

    Time: 15 ms (30.19%), Space: 32.3 MB (57.55%) - LeetHub

--- 3621-minimum-operations-to-make-array-values-equal-to-k/3621-minimum-operations-to-make-array-values-equal-to-k.cpp
+++ 3621-minimum-operations-to-make-array-values-equal-to-k/3621-minimum-operations-to-make-array-values-equal-to-k.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums, int k) {
-        unordered_map<int, int> mpp;
-        for (int i : nums)
-            if (i < k)
-                return -1;
-            else if (i > k)
-                mpp[i]++;
-        return mpp.size();
+        int mini = *min_element(nums.begin(), nums.end());
+        if (mini < k)
+            return -1;
+
+        unordered_set<int> st(nums.begin(), nums.end());
+        return st.size() - (mini == k);
     }
 };
