83aaf5b67157445b3d32ab189c797c8fd2d5d3ad
Aditya <150531010+idityaGE@users.noreply.github.com>
2026-02-09 23:02:43 +0530

    0109-CONVERT-SORTED-LIST-TO-BINARY-SEARCH-TREE Stats: Time: 2 ms (21.43%), Space: 7.8 MB (96.43%) - LeetHub

--- 0109-convert-sorted-list-to-binary-search-tree/0109-convert-sorted-list-to-binary-search-tree.go
+++ 0109-convert-sorted-list-to-binary-search-tree/0109-convert-sorted-list-to-binary-search-tree.go
@@ -1,53 +1,25 @@
-/**
- * Definition for singly-linked list.
- * type ListNode struct {
- *     Val int
- *     Next *ListNode
- * }
- */
-/**
- * Definition for a binary tree node.
- * type TreeNode struct {
- *     Val int
- *     Left *TreeNode
- *     Right *TreeNode
- * }
- */
 func sortedListToBST(head *ListNode) *TreeNode {
     if head == nil {
         return nil
     }
-
-	arr := LlToArr(head)
-
-	return construct(arr, 0, len(arr)-1)
-}
-
-func construct(arr []int, l, r int) *TreeNode {
-	if l > r {
-		return nil
-	}
-
-	mid := l + (r-l)/2
-	node := &TreeNode{}
-
-	node.Val = arr[mid]
-
-	node.Left = construct(arr, l, mid-1)
-	node.Right = construct(arr, mid+1, r)
-
-	return node
-}
-
-func LlToArr(head *ListNode) []int {
-	arr := make([]int, 0)
-
-	temp := head
-
-	for temp != nil {
-		arr = append(arr, temp.Val)
-		temp = temp.Next
-	}
-
-	return arr
+    if head.Next == nil {
+        return &TreeNode{Val: head.Val}
+    }
+    
+    var prev *ListNode
+    slow, fast := head, head
+    for fast != nil && fast.Next != nil {
+        prev = slow
+        slow = slow.Next
+        fast = fast.Next.Next
+    }
+    
+    // Disconnect left half
+    prev.Next = nil
+    
+    node := &TreeNode{Val: slow.Val}
+    node.Left = sortedListToBST(head)
+    node.Right = sortedListToBST(slow.Next)
+    
+    return node
 }
