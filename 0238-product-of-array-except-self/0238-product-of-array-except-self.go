26ceb2bfde63cafdc0609ecaa0274c667588598e
Aditya <150531010+idityaGE@users.noreply.github.com>
2026-02-19 23:17:24 +0530

    0238-PRODUCT-OF-ARRAY-EXCEPT-SELF Stats: Time: 0 ms (100%), Space: 10.9 MB (22.73%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.go
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.go
@@ -1,35 +1,17 @@
 func productExceptSelf(nums []int) []int {
-    n := len(nums)
+	n := len(nums)
+	output := make([]int, n)
 
-    if n == 2 {
-        return []int{nums[1], nums[0]}
-    }
+	output[0] = 1
+	for i := 1; i < n; i++ {
+		output[i] = output[i-1] * nums[i-1]
+	}
 
-    pre := make([]int, n)
-    suf := make([]int, n)
+	right := 1
+	for i := n - 1; i >= 0; i-- {
+		output[i] *= right
+		right *= nums[i]
+	}
 
-    pre[0] = nums[0]
-    suf[n - 1] = nums[n - 1]
-
-    for i := 1; i < n; i++ {
-        pre[i] = nums[i] * pre[i -1]
-    }
-
-    for i := n - 2; i >= 0; i-- {
-        suf[i] = nums[i] * suf[i + 1]
-    }
-
-    // fmt.Println(pre)
-    // fmt.Println(suf)
-
-    res := make([]int, n)
-
-    res[0] = suf[1]
-    res[n - 1] = pre[n - 2]
-
-    for i := 1; i < n - 1; i++ {
-        res[i] = pre[i - 1] * suf[i + 1]
-    }
-
-    return res
+	return output
 }
