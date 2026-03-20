d3ddc5d435fb34a86731a49a22f18278634af3b0
Aditya <150531010+idityaGE@users.noreply.github.com>
2026-03-20 21:37:43 +0530

    1951-FIND-THE-WINNER-OF-THE-CIRCULAR-GAME Stats: Time: 0 ms (100%), Space: 7.8 MB (78.3%) - LeetHub

--- 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.cpp
+++ 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.cpp
@@ -1,18 +1,13 @@
 class Solution {
 public:
     int findTheWinner(int n, int k) {
-        vector<int> arr (n, 0);
+        return solve(n, k) + 1;
+    }
 
-        for(int i = 1; i <= n; i++) arr[i - 1] = i;
-
-        int i = 0;
-
-        while(arr.size() > 1) {
-            int idx = (i + k - 1) % arr.size();
-            arr.erase(arr.begin() + idx);
-            i = idx;
-        }
-
-        return arr[0];
+    int solve(int n, int k) {
+        if (n==1) return 0;
+        int idx = solve(n-1, k);
+        idx = (idx + k) % n;
+        return idx;
     }
 };
