0b63a42095795808a7801e5bbf0feb8117bfdee7
Aditya <150531010+idityaGE@users.noreply.github.com>
2026-06-11 23:09:13 +0530

    0111-MINIMUM-DEPTH-OF-BINARY-TREE Stats: Time: 0 ms (100%), Space: 147 MB (5.25%) - LeetHub

--- 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.cpp
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.cpp
@@ -6,16 +6,17 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
     int minDepth(TreeNode* root) {
-        if (!root) return 0;
+        if (!root)
+            return 0;
 
         queue<pair<TreeNode*, int>> q;
-        int minDepth = INT_MAX;
 
         q.push({root, 1});
 
@@ -23,18 +24,18 @@ public:
             pair<TreeNode*, int> temp = q.front();
             q.pop();
 
-            if (!temp.first->left && !temp.first->right) {
-                minDepth = min(minDepth, temp.second);
-            } else {
-                if (temp.first->left) {
-                    q.push({temp.first->left, temp.second+1});
-                }
-                if (temp.first->right) {
-                    q.push({temp.first->right, temp.second+1});
-                }
-            }
+            if (!temp.first->left && !temp.first->right) 
+                return temp.second;
+            
+
+            if (temp.first->left) 
+                q.push({temp.first->left, temp.second + 1});
+            
+            if (temp.first->right) 
+                q.push({temp.first->right, temp.second + 1});
+        
         }
 
-        return minDepth;
+        return 0;
     }
 };
