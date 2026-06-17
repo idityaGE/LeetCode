d7c54ee748c22c7e6aa62fee7b60c846eed4ce58
Aditya <150531010+idityaGE@users.noreply.github.com>
2026-06-17 20:10:17 +0530

    1013-FIBONACCI-NUMBER Stats: Time: 1 ms (81.8%), Space: 3.8 MB (64.94%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.go
+++ 1013-fibonacci-number/1013-fibonacci-number.go
@@ -3,9 +3,21 @@ func fib(n int) int {
         return 0
     }
 
+    memo := make([]int, n+1)
+
+    return solve(n, memo)
+}
+
+func solve(n int, memo []int) int {
+    if memo[n] != 0 {
+        return memo[n]
+    }
+
     if n <= 2 {
         return 1
     }
 
-    return fib(n-1) + fib(n-2)
+    memo[n] = solve(n -1, memo) + solve(n-2, memo)
+
+    return memo[n]
 }
