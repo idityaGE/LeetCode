2a174af7acb1064eed9ee3cd49d822de27cefb0d
Aditya <150531010+idityaGE@users.noreply.github.com>
2026-06-18 22:02:15 +0530

    1446-ANGLE-BETWEEN-HANDS-OF-A-CLOCK Stats: Time: 0 ms (100%), Space: 3.9 MB (60%) - LeetHub

--- 1446-angle-between-hands-of-a-clock/1446-angle-between-hands-of-a-clock.go
+++ 1446-angle-between-hands-of-a-clock/1446-angle-between-hands-of-a-clock.go
@@ -1,6 +1,10 @@
 import "math"
 
 func angleClock(hour int, minutes int) float64 {
+    // 360 / 60 = 6 deg / min
+    // 360 / 12 = 30 deg / hours 
+    // 30 / 6 = 0.5 deg / per min
+
     mindeg := float64(minutes) * 6.0
 
     hourdeg := float64(hour%12)*30.0 + float64(minutes)*0.5
