aeb8cf55fe889a567bfbfe434894b754b783a816
Aditya <150531010+idityaGE@users.noreply.github.com>
2026-06-22 22:04:56 +0530

    0144-BINARY-TREE-PREORDER-TRAVERSAL Stats: Time: 0 ms (100%), Space: 4 MB (99.46%) - LeetHub

---
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.go
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func preorderTraversal(root *TreeNode) []int {
+    ans := []int{}
+
+    var dfs func(*TreeNode)
+    dfs = func(node *TreeNode) {
+        if node == nil {
+            return
+        }
+
+        ans = append(ans, node.Val)
+
+        dfs(node.Left)
+        dfs(node.Right)
+    }
+
+    dfs(root)
+
+    return ans
+}
